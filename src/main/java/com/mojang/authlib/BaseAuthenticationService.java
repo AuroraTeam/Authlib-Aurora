3c63dfb976f7639cec1cc25dec4db9a350ac75c3
KeeperJerry <keeperjerry@yandex.ru>
2021-01-16 05:56:11 +0300

    gays authlib

---
+++ src/main/java/com/mojang/authlib/BaseAuthenticationService.java
@@ -0,0 +1,6 @@
+package com.mojang.authlib;
+
+public abstract class BaseAuthenticationService implements AuthenticationService {
+   public BaseAuthenticationService() {
+   }
+}
---
+++ src/main/java/com/mojang/authlib/minecraft/OfflineSocialInteractions.java
@@ -0,0 +1,24 @@
+package com.mojang.authlib.minecraft;
+
+import java.util.UUID;
+
+public class OfflineSocialInteractions implements SocialInteractionsService {
+   public OfflineSocialInteractions() {
+   }
+
+   public boolean serversAllowed() {
+      return true;
+   }
+
+   public boolean realmsAllowed() {
+      return true;
+   }
+
+   public boolean chatAllowed() {
+      return true;
+   }
+
+   public boolean isBlockedPlayer(UUID playerID) {
+      return false;
+   }
+}
